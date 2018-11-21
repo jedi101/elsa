40daff6720c9c9fada89921d90636aeee178569b
Tobias Koppmann <tobias.koppmann@gmail.com>
2018-11-21 11:28:20 +0100

    fix/improve comments

--- eLSA_Robot/Src/main.cpp
+++ eLSA_Robot/Src/main.cpp
@@ -12,12 +12,14 @@
 /* main function -------------------------------------------------------------*/
 int main(void)
 {
-  /* Infinite loop */
-  msp_system_init();
 
-  while (1)
-  {
+	/*initialize mcu system and peripherals*/
+	msp_system_init();
+
+	/* Infinite loop */
+	while (1)
+	{
 	  HAL_GPIO_TogglePin(GPIOA, LD2_Pin);
 	  HAL_Delay(1000);
-  }
+	}
 }
