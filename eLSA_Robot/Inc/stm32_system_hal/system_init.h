32f3c82f342878107af31881e9a943d78c20a7b0
Tobias Koppmann <tobias.koppmann@gmail.com>
2018-11-21 11:26:34 +0100

    bundle calls of system init functions  in msp_system_init()

---
+++ eLSA_Robot/Inc/stm32_system_hal/system_init.h
@@ -0,0 +1,15 @@
+
+#ifndef __SYSTEM_INIT_H
+#define __SYSTEM_INIT_H
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/*function prototypes*/
+ void msp_system_init(void);
+void SystemClock_Config(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /*__SYSTEM_INIT_H */
