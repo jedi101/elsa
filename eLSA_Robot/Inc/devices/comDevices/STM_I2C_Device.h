14f12967be29f041ee431dba35dbdfe3d4cfe361
Tobias Koppmann <tobias.koppmann@gmail.com>
2018-12-04 16:51:13 +0100

    switch STM_I2C_Device to abstract base class and refactor methods

--- eLSA_Robot/Inc/devices/comDevices/STM_I2C_Device.h
+++ eLSA_Robot/Inc/devices/comDevices/STM_I2C_Device.h
@@ -2,6 +2,7 @@
 #ifndef ELSA_I2CDEVICE_H_
 #define ELSA_I2CDEVICE_H_
 
+#include <comDevices/IComDriver.h>
 #include <cstdint>
 #include "i2c.h"
 
@@ -15,13 +16,17 @@ namespace eLSA {
  *  A device object is bound to a specific bus address and therefore representing a single device.
  *  The class provides standard read/write methods for common I2C operations.
  */
-class STM_I2C_Device /*: I2C_Device*/ {
+class STM_I2C_Device : public hwInterfaces::IComDriver {
 public:
+
+	//delete the standard constructor, so that it is impossible to create unusable objects
+	STM_I2C_Device() = delete;
+
 	/**
 	 * @param i2c_port A STM32 HAL handle describing the device
 	 * @param device_address The bus address of the device which an object of this class is representing
 	 */
-	STM_I2C_Device(I2C_HandleTypeDef* i2c_port, uint16_t device_address);
+	STM_I2C_Device(I2C_HandleTypeDef* i2cPort, uint16_t deviceAddress);
 
 	/**
 	 * Just a standard deconstructor
@@ -29,35 +34,34 @@ public:
 	virtual ~STM_I2C_Device();
 
 	/**
-	 * @param pAddressCommand Pointer to a variable containing the address value to which is written
-	 * @param sizeAddressCommand Size of the given address value. This value is explicit to make 11Bit I2C adressing available
-	 * @param pCommand Pointer to a variable containing the command value which is written
-	 * @param sizeCommand Size of the command which is written
-	 * @param timeout Timeout for the interface in milliseconds
+	 * @param pRegisterAddress Pointer to a variable containing the address value to which is written
+	 * @param sizeRegisterAddress Size of the given address value. This value is explicit to make 11Bit I2C adressing available
 	 */
-	unsigned int writeCommand(uint16_t pAddressCommand, uint16_t sizeAddressCommand, uint8_t* pCommand, uint16_t sizeCommand, uint32_t timeout);
+	int setDeviceRegisterParams(uint16_t pRegisterAddress, uint16_t sizeRegisterAddress);
 
 	/**
-	 * @param pAddressData Pointer to a variable containing the address value to which is written
-	 * @param sizeAddressData Size of the given address value. This value is explicit to make 11Bit I2C adressing available
+	 * @param time Desired connection timeout in milliseconds
+	 */
+	void setConnectionTimeout(uint32_t time);
+
+	/**
 	 * @param pData Pointer to the start address of the data which is written
 	 * @param sizeData Size of the data block which is written
-	 * @param timeout Timeout for the interface in milliseconds
 	 */
-	unsigned int writeData(uint16_t pAddressData, uint16_t sizeAddressData, uint8_t* pData, uint16_t sizeData, uint32_t timeout);
+	int writeData(uint8_t* pData, uint16_t sizeData);
 
 	/**
-	 * @param pAddressData Pointer to a variable containing the address value from which is read
-	 * @param sizeAddressData Size of the given address value. This value is explicit to make 11Bit I2C adressing available
 	 * @param pData Pointer to the start address of the data buffer to which the read data is written
 	 * @param sizeData Size of the data block which is read
-	 * @param timeout Timeout for the interface in milliseconds
 	 */
-	unsigned int readData(uint16_t pAddressData, uint16_t sizeAddressData, uint8_t* pData, uint16_t sizeData, uint32_t timeout);
+	int readData(uint8_t* pData, uint16_t sizeData);
 
 private:
-	I2C_HandleTypeDef* _i2c_port;
-	uint16_t _device_address;
+	I2C_HandleTypeDef* _i2cPort;
+	uint16_t _deviceAddress;
+	uint16_t _pRegisterAddress;
+	uint16_t _sizeRegisterAddress;
+	uint32_t _timeout = 100;
 };
 
 } /* namespace eLSA */
--- eLSA_Robot/Src/comDevices/STM_I2C_Device.cpp
+++ eLSA_Robot/Src/comDevices/STM_I2C_Device.cpp
@@ -11,56 +11,70 @@
 namespace eLSA {
 
 	//constructor for I2C device
-	STM_I2C_Device::STM_I2C_Device(I2C_HandleTypeDef* i2c_port, uint16_t device_address)
-		:_i2c_port{i2c_port}, _device_address{device_address}{}
+	STM_I2C_Device::STM_I2C_Device(I2C_HandleTypeDef* i2cPort, uint16_t deviceAddress)
+		:_i2cPort{i2cPort}, _deviceAddress{deviceAddress}{}
 
 	//destructor for I2C device
 	STM_I2C_Device::~STM_I2C_Device(){};
 
-	// write command to I2C device
-	unsigned int STM_I2C_Device::writeCommand(uint16_t pAddressCommand, uint16_t sizeAddressCommand, uint8_t* pCommand, uint16_t sizeCommand, uint32_t timeout)
-	{
-		return STM_I2C_Device::writeData(pAddressCommand, sizeAddressCommand, pCommand, sizeCommand, timeout);
+	//set register address used for writing or reading
+	int STM_I2C_Device::setDeviceRegisterParams(uint16_t pRegisterAddress, uint16_t sizeRegisterAddress){
+		int returnValue = -1;
+
+		if(pRegisterAddress && sizeRegisterAddress) {
+			this->_pRegisterAddress = pRegisterAddress;
+			this->_sizeRegisterAddress = sizeRegisterAddress;
+			returnValue = 0;
+		}
+
+		return returnValue;
+	}
+
+	//set connection timeout
+	void STM_I2C_Device::setConnectionTimeout(uint32_t time){
+		if(time) {
+			this->_timeout = time;
+		}
 	}
 
 	// write data to I2C device
-	unsigned int STM_I2C_Device::writeData(uint16_t pAddressDataRegister, uint16_t sizeAddressData, uint8_t* pData, uint16_t sizeData, uint32_t timeout)
+	int STM_I2C_Device::writeData(uint8_t* pData, uint16_t sizeData)
 	{
 		HAL_StatusTypeDef _status = HAL_BUSY;
 
 		//check if device is ready
 		do {
-			_status = HAL_I2C_IsDeviceReady(_i2c_port, _device_address,5,timeout);
+			_status = HAL_I2C_IsDeviceReady(_i2cPort, _deviceAddress,5,_timeout);
 		} while(_status == HAL_BUSY);
 
 		//only try to send data, when device is ready
 		if(_status == HAL_OK) {
 			do {
-				_status = HAL_I2C_Mem_Write(_i2c_port, _device_address, pAddressDataRegister, sizeAddressData, pData, sizeData, timeout);
+				_status = HAL_I2C_Mem_Write(_i2cPort, _deviceAddress, _pRegisterAddress, _sizeRegisterAddress, pData, sizeData, _timeout);
 			} while(_status == HAL_BUSY);
 		}
 
-		return _status;
+		return (int)_status;
 	}
 
 	// read data from I2C device
-	unsigned int STM_I2C_Device::readData(uint16_t pAddressDataRegister, uint16_t sizeAddressData, uint8_t* pData, uint16_t sizeData, uint32_t timeout)
+	int STM_I2C_Device::readData(uint8_t* pData, uint16_t sizeData)
 	{
 		HAL_StatusTypeDef _status = HAL_BUSY;
 
 		//check if device is ready
 		do {
-			_status = HAL_I2C_IsDeviceReady(_i2c_port, _device_address,5,timeout);
+			_status = HAL_I2C_IsDeviceReady(_i2cPort, _deviceAddress,5,_timeout);
 		} while(_status == HAL_BUSY);
 
 		//only try to read data, when device is ready
 		if(_status == HAL_OK) {
 			do {
-				_status = HAL_I2C_Mem_Read(_i2c_port, _device_address, pAddressDataRegister, sizeAddressData, pData, sizeData, timeout);
+				_status = HAL_I2C_Mem_Read(_i2cPort, _deviceAddress, _pRegisterAddress, _sizeRegisterAddress, pData, sizeData, _timeout);
 			} while(_status == HAL_BUSY);
 		}
 
-		return _status;
+		return (int)_status;
 	}
 
 
