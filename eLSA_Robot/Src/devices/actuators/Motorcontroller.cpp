8bfa5e12d3bd9bb280f79683bc38c1a250c32784
Tobias Koppmann <tobias.koppmann@gmail.com>
2019-01-06 17:13:26 +0100

    implement constructor of Motorcontroller

--- eLSA_Robot/Src/devices/actuators/Motorcontroller.cpp
+++ eLSA_Robot/Src/devices/actuators/Motorcontroller.cpp
@@ -13,10 +13,27 @@ namespace actuators {
 // initialize singleton instance
 Motorcontroller* Motorcontroller::_instance = 0;
 
+//Constructor which is called in instance()
+Motorcontroller::Motorcontroller(I2C_HandleTypeDef* i2cPort, uint16_t deviceAddress)
+	: _i2cPort{i2cPort}, _i2cAddress{deviceAddress}
+{
+	if(_i2cPort && _i2cAddress) {
+
+		//retrieve new i2c interface object
+		_i2cInterface = new eLSA::comDevices::StmI2cDevice(_i2cPort, _i2cAddress);
+
+		if(_i2cInterface) {
+
+			//set display standard timeout for i2c device
+			_i2cInterface->setConnectionTimeout(MC_I2C_TIMEOUT);
+
+		}
+	}
+
 }
 
 Motorcontroller::~Motorcontroller() {
-	// TODO Auto-generated destructor stub
+
 }
 
 } /* namespace actuators */
