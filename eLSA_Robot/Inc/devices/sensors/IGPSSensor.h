b415fdda227f86a46e35858416b303175cf22cbc
Tobias Koppmann <tobias.koppmann@gmail.com>
2019-01-11 14:47:52 +0100

    combine struct and interface definition for GPS sensor

--- eLSA_Robot/Inc/devices/sensors/IGPSSensor.h
+++ eLSA_Robot/Inc/devices/sensors/IGPSSensor.h
@@ -13,8 +13,29 @@
 namespace eLSA {
 namespace sensors {
 
+/**
+ * @author Richard Leikam
+ * @date 04.12.2018
+ * This struct is used to represent a GPS position
+ */
+typedef struct {
+	float longitude; ///< The longitude given in dddmm.mmmmmm where d=degree and m=minutes
+	float latitude; ///< ///< The latitude given in ddmm.mmmmmm where d=degree and m=minutes
+} GPSPoint_t;
+
+/**
+ * @author Richard Leikam
+ * @date 04.12.2018
+ *
+ * @brief This interface is used to generalise a GPS sensor and provide a unified hardware abstraction as specified for
+ * the eLSA system
+ */ 
 class IGPSSensor {
 public:
+    /**
+    * @brief This method is used to get the current GPS position as measured by the sensor
+    * @return A GPS-Point struct variable containing longitude as well as latitude
+    */
 	virtual GPSPoint_t getGPSPoint() = 0;
 };
 
