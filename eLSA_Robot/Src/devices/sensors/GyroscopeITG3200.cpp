72355b3af9462893e8aa7857a855fa0a8a4314e0
Tobias Koppmann <tobias.koppmann@gmail.com>
2019-01-13 17:16:55 +0100

    add class for Gyroscope ITG3200

---
+++ eLSA_Robot/Src/devices/sensors/GyroscopeITG3200.cpp
@@ -0,0 +1,23 @@
+/*
+ * GyroscopeITG3200.cpp
+ *
+ *  Created on: 13.01.2019
+ *      Author: Tobias
+ */
+
+#include <sensors/GyroscopeITG3200.h>
+
+namespace eLSA {
+namespace sensors {
+
+GyroscopeITG3200::GyroscopeITG3200() {
+	// TODO Auto-generated constructor stub
+
+}
+
+GyroscopeITG3200::~GyroscopeITG3200() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace sensors */
+} /* namespace eLSA */
