3a42d44412ae62736ac965796dd650730addfb68
Tobias Koppmann <tobias.koppmann@gmail.com>
2019-01-18 09:12:46 +0100

    add class ConditionMonitor

---
+++ eLSA_Robot/Inc/ConditionMonitoring/ConditionMonitor.h
@@ -0,0 +1,21 @@
+/*
+ * ConditionMonitor.h
+ *
+ *  Created on: 18.01.2019
+ *      Author: Tobias
+ */
+
+#ifndef CONDITIONMONITORING_CONDITIONMONITOR_H_
+#define CONDITIONMONITORING_CONDITIONMONITOR_H_
+
+namespace eLSA {
+
+class ConditionMonitor {
+public:
+	ConditionMonitor();
+	virtual ~ConditionMonitor();
+};
+
+} /* namespace eLSA */
+
+#endif /* CONDITIONMONITORING_CONDITIONMONITOR_H_ */
---
+++ eLSA_Robot/Src/ConditionMonitoring/ConditionMonitor.cpp
@@ -0,0 +1,21 @@
+/*
+ * ConditionMonitor.cpp
+ *
+ *  Created on: 18.01.2019
+ *      Author: Tobias
+ */
+
+#include <ConditionMonitoring/ConditionMonitor.h>
+
+namespace eLSA {
+
+ConditionMonitor::ConditionMonitor() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ConditionMonitor::~ConditionMonitor() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace eLSA */
