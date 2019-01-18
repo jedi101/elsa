970d7c1eb9904481f37e07ef8af58318fc29fa02
Tobias Koppmann <tobias.koppmann@gmail.com>
2019-01-18 09:13:23 +0100

    add class HazardDetector

---
+++ eLSA_Robot/Inc/HazardDetection/HazardDetector.h
@@ -0,0 +1,21 @@
+/*
+ * HazardDetection.h
+ *
+ *  Created on: 18.01.2019
+ *      Author: Tobias
+ */
+
+#ifndef HAZARDDETECTION_HAZARDDETECTOR_H_
+#define HAZARDDETECTION_HAZARDDETECTOR_H_
+
+namespace eLSA {
+
+class HazardDetector {
+public:
+	HazardDetector();
+	virtual ~HazardDetector();
+};
+
+} /* namespace eLSA */
+
+#endif /* HAZARDDETECTION_HAZARDDETECTOR_H_ */
---
+++ eLSA_Robot/Src/HazardDetection/HazardDetector.cpp
@@ -0,0 +1,21 @@
+/*
+ * HazardDetection.cpp
+ *
+ *  Created on: 18.01.2019
+ *      Author: Tobias
+ */
+
+#include <HazardDetection/HazardDetector.h>
+
+namespace eLSA {
+
+HazardDetector::HazardDetector() {
+	// TODO Auto-generated constructor stub
+
+}
+
+HazardDetector::~HazardDetector() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace eLSA */
