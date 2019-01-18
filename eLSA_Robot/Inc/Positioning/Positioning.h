dab3c72cd431e6247e0b6d8d7437f1e945a9f201
Tobias Koppmann <tobias.koppmann@gmail.com>
2019-01-18 09:13:40 +0100

    add class Positioning

---
+++ eLSA_Robot/Inc/Positioning/Positioning.h
@@ -0,0 +1,21 @@
+/*
+ * Positioning.h
+ *
+ *  Created on: 18.01.2019
+ *      Author: Tobias
+ */
+
+#ifndef POSITIONING_POSITIONING_H_
+#define POSITIONING_POSITIONING_H_
+
+namespace eLSA {
+
+class Positioning {
+public:
+	Positioning();
+	virtual ~Positioning();
+};
+
+} /* namespace eLSA */
+
+#endif /* POSITIONING_POSITIONING_H_ */
---
+++ eLSA_Robot/Src/Positioning/Positioning.cpp
@@ -0,0 +1,21 @@
+/*
+ * Positioning.cpp
+ *
+ *  Created on: 18.01.2019
+ *      Author: Tobias
+ */
+
+#include <Positioning/Positioning.h>
+
+namespace eLSA {
+
+Positioning::Positioning() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Positioning::~Positioning() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace eLSA */
