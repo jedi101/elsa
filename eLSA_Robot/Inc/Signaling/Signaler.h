776f23b890e47a93b638857920a57264afca2337
Tobias Koppmann <tobias.koppmann@gmail.com>
2019-01-18 09:13:52 +0100

    add class Signaler

---
+++ eLSA_Robot/Inc/Signaling/Signaler.h
@@ -0,0 +1,21 @@
+/*
+ * Signaler.h
+ *
+ *  Created on: 18.01.2019
+ *      Author: Tobias
+ */
+
+#ifndef SIGNALING_SIGNALER_H_
+#define SIGNALING_SIGNALER_H_
+
+namespace eLSA {
+
+class Signaler {
+public:
+	Signaler();
+	virtual ~Signaler();
+};
+
+} /* namespace eLSA */
+
+#endif /* SIGNALING_SIGNALER_H_ */
---
+++ eLSA_Robot/Src/Signaling/Signaler.cpp
@@ -0,0 +1,21 @@
+/*
+ * Signaler.cpp
+ *
+ *  Created on: 18.01.2019
+ *      Author: Tobias
+ */
+
+#include <Signaling/Signaler.h>
+
+namespace eLSA {
+
+Signaler::Signaler() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Signaler::~Signaler() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace eLSA */
