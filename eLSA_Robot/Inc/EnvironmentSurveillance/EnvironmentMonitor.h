e99c1be782f36a86107d4d30d015f50ba874a5a7
Tobias Koppmann <tobias.koppmann@gmail.com>
2019-01-18 09:13:04 +0100

    add class EnvironmentMonitor

---
+++ eLSA_Robot/Inc/EnvironmentSurveillance/EnvironmentMonitor.h
@@ -0,0 +1,21 @@
+/*
+ * EnvironmentSurveillance.h
+ *
+ *  Created on: 18.01.2019
+ *      Author: Tobias
+ */
+
+#ifndef ENVIRONMENTSURVEILLANCE_ENVIRONMENTMONITOR_H_
+#define ENVIRONMENTSURVEILLANCE_ENVIRONMENTMONITOR_H_
+
+namespace eLSA {
+
+class EnvironmentMonitor {
+public:
+	EnvironmentMonitor();
+	virtual ~EnvironmentMonitor();
+};
+
+} /* namespace eLSA */
+
+#endif /* ENVIRONMENTSURVEILLANCE_ENVIRONMENTMONITOR_H_ */
---
+++ eLSA_Robot/Src/EnvironmentSurveillance/EnvironmentMonitor.cpp
@@ -0,0 +1,21 @@
+/*
+ * EnvironmentSurveillance.cpp
+ *
+ *  Created on: 18.01.2019
+ *      Author: Tobias
+ */
+
+#include <EnvironmentSurveillance/EnvironmentMonitor.h>
+
+namespace eLSA {
+
+EnvironmentMonitor::EnvironmentMonitor() {
+	// TODO Auto-generated constructor stub
+
+}
+
+EnvironmentMonitor::~EnvironmentMonitor() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace eLSA */
